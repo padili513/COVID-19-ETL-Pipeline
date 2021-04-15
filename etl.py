6caf954127be033ee0f190356be58417ded2ed19
Pedram Adili <pedramadili@gmail.com>
2021-04-15 09:50:38 -0400

    Add function for SNS notification

--- etl.py
+++ etl.py
@@ -6,12 +6,24 @@ import pandas as pd
 from dynamodb_json import json_util as json
 import numpy as np
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.info(event)
+logger.info(context)
+
+# Initialize boto3 clients
+sns = boto3.client('sns')
+
+def notify(text):
+    try:
+        sns.publish(TopicArn=os.environ['sns_topic'],
+                    Subject='Covid19 ETL summary',
+                    Message=text)
+    except Exception as e:
+        logger.error("Sending notification failed: {}".format(e))
+        exit(1)
+    logger.info(text)
+
 def lambda_handler(event, context):
 
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    logger.info(event)
-    logger.info(context)
-
-    # Initialize boto3 clients
-    sns = boto3.client('sns')
+    
